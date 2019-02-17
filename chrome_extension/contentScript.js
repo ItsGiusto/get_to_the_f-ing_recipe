ccb49593f5da23007aacd0930908949fd68c6380
ItsGiusto <giustobgood@optonline.net>
2019-02-17 00:25:01 -0500

    works for greasemonkey, firefox, and chrome

---
+++ chrome_extension/contentScript.js
@@ -0,0 +1,21 @@
+var classTermArray = ["wprm-recipe-container", "tasty-recipes-display"];
+
+function setupRecipeActions() {
+    for (var i = 0; i < classTermArray.length; i++) {
+        moveToTop(classTermArray[i]);
+    }
+}
+
+function moveToTop(className){
+    var wprmRecipeContainerArray = document.getElementsByClassName(className);
+    for (var i = 0; i < wprmRecipeContainerArray.length; i++) {
+        var wprmRecipeContainer = wprmRecipeContainerArray[i];
+        var parent = wprmRecipeContainer.parentElement;
+        if(parent){
+            var firstChild = parent.firstChild;
+            parent.insertBefore(wprmRecipeContainer, firstChild);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded',  setupRecipeActions(), false);
---
+++ greasemonkey_script.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Get to the F***ing Recipe
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Move recipes, elements matching terms in the classTermArray, to the top of its parent element
+// @author       ItsGiusto
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+var classTermArray = ["wprm-recipe-container", "tasty-recipes-display"];
+
+function setupRecipeActions() {
+    for (var i = 0; i < classTermArray.length; i++) {
+        moveToTop(classTermArray[i]);
+    }
+}
+
+function moveToTop(className){
+    var wprmRecipeContainerArray = document.getElementsByClassName(className);
+    for (var i = 0; i < wprmRecipeContainerArray.length; i++) {
+        var wprmRecipeContainer = wprmRecipeContainerArray[i];
+        var parent = wprmRecipeContainer.parentElement;
+        if(parent){
+            var firstChild = parent.firstChild;
+            parent.insertBefore(wprmRecipeContainer, firstChild);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded',  setupRecipeActions(), false);
